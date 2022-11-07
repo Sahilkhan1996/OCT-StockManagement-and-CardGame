f6effb3af5bb93cef60cae52fed872cf1c29dd19
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-07 18:38:04 +0530

    Adding the program of Deck of cards and stock management

---
+++ src/com/bridgelabz/deckofcard/CardGame.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.deckofcard;
+
+public class CardGame {
+
+	public static void main(String[] args) {
+
+		DistributeCard distributeCard = new DistributeCard(4, 4);
+
+		distributeCard.distribute();
+
+		distributeCard.printDistributedCards();
+	}
+
+}
